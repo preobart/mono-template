8d254189cdd2e1676b904b293aeddb24eebbaa3f
Artemii Preobrazhenskii <preobart@gmail.com>
2025-09-07 18:54:11 +0300

    v0.2.0

---
+++ backend/project_name/celery_app.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from celery import Celery
+
+
+settings_module = os.environ.get("DJANGO_SETTINGS_MODULE", default=None)
+if settings_module is None:
+    print(
+        "Error: no DJANGO_SETTINGS_MODULE found. Will NOT start devserver. "
+        "Remember to create .env file at project root. "
+        "Check README for more info."
+    )
+    sys.exit(1)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+app = Celery("{{ project_name }}_tasks")  # type: ignore
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
